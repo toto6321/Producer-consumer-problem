fcfb12bd090bb64eab97eda3ae96b80b4a047b4a
toto <toto6321@outlook.com>
2018-11-20 16:34:38 -0500

    FEATURE: add elapsed time measurement codes for non-busy-waiting implementation. TODO: more precise time measurement need to be conduct

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include <pthread.h>
+#include <time.h>
 
 #define  N_BUFFER 2
 #define  N_THREAD 3
+#define  N_LOOP 3
 
 // each pair of empty and full semaphores controls one buffer
 sem_t empty[N_BUFFER];
@@ -16,10 +18,16 @@ sem_t mutex[2];
 char buffer[N_BUFFER] = {'a', 'a'};
 char labels[N_THREAD] = {'A', 'B', 'C'};
 
+time_t timestamp[N_LOOP + 1];
+
+// how many times it loops (one loop starts from A produces one to C consumes it)
+int count = 0;
+
+
 void *processA(void *v) {
     char *l = (char *) v;
 
-    while (1) {
+    while (count < N_LOOP - 1) {
         sem_wait(&empty[0]);
         sem_wait(&mutex[0]);
         printf("Process A is writing A into buffer1\n");
@@ -33,7 +41,7 @@ void *processA(void *v) {
 void *processB(void *v) {
     char *l = (char *) v;
 
-    while (1) {
+    while (count < N_LOOP - 1) {
         sem_wait(&full[0]);
         sem_wait(&mutex[0]);
         char buffer1 = buffer[0];
@@ -57,7 +65,7 @@ void *processB(void *v) {
 void *processC(void *v) {
     char *l = (char *) v;
 
-    while (1) {
+    while (count < N_LOOP ) {
         sem_wait(&full[1]);
         sem_wait(&mutex[1]);
         char buffer2 = buffer[1];
@@ -65,6 +73,9 @@ void *processC(void *v) {
         sleep(2);
         sem_post(&mutex[1]);
         sem_post(&empty[1]);
+
+        // increase the counter by 1 and record current timestamp
+        time(&timestamp[++count]);
     }
 }
 
@@ -85,6 +96,9 @@ int main(int argc, char **argv) {
 
     pthread_t threads[N_THREAD];
     int thread_ids[N_THREAD];
+
+    // start to count
+    time(&timestamp[0]);
     thread_ids[0] = pthread_create(&threads[0], NULL, processA, &labels[0]);
     thread_ids[1] = pthread_create(&threads[1], NULL, processB, &labels[1]);
     thread_ids[2] = pthread_create(&threads[2], NULL, processC, &labels[2]);
@@ -92,6 +106,22 @@ int main(int argc, char **argv) {
     for (int i = 0; i < N_THREAD; i++) {
         pthread_join(threads[i], NULL);
     }
+
+    /* write the elapsed time into file */
+    long int elapsed_time[N_LOOP];
+    for (int i = 0; i < N_LOOP; i++) {
+        elapsed_time[i] = timestamp[i + 1] - timestamp[i];
+    }
+    char filename[50] = "statistics_non-busy-waiting.csv";
+    FILE *write_stream = fopen(filename, "a");
+    for (int i = 0; i < N_LOOP; i++) {
+        if (i == N_LOOP - 1) {
+            fprintf(write_stream, "%ld\n", elapsed_time[i]);
+        } else {
+            fprintf(write_stream, "%ld,", elapsed_time[1]);
+        }
+    }
+
 
     return 0;
 }
